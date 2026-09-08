eee8c252e0732afec264fdbc90ae7d3a3f931e04
iamtheaashish <iamtheaashish@gmail.com>
2026-09-08 10:38:37 +0530

    update main.go to redefine variable x as int and calculate sums with y

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	var x rune = 'a';
-	fmt.Println(x);
+	var x int = 10;
+	var y float64 = 30.2;
+	var sum1 float64 = float64(x) + y;
+	var sum2 int = x + int(y);
+	fmt.Println(sum1,sum2);
 }
