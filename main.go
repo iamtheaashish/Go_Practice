784d8afd09d08e411fa4ab475b9ae53ccbe6f771
iamtheaashish <iamtheaashish@gmail.com>
2026-09-08 16:59:02 +0530

    update main.go to redefine constant x and adjust formatting

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+const x int64 = 10
+const (
+	idKey = "id"
+	nameKey = "name"
+)
+
+const z = 20 * 10
+
 func main(){
-	var x int = 10;
+	const y = "hello"
+
+	fmt.Println(x)
+	fmt.Println(y)
 }
