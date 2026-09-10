4a3d133931fe93e8750248a5abd77dd94283d962
Aashish Raj <iamtheaashish@gmail.com>
2026-09-10 08:44:21 +0530

    update hello.go to redefine variables using a constant and print integer and float values

--- ch1/hello.go
+++ ch1/hello.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	var i int = 20
-	var f float64 = float64(i)
+	const value = 20
+	var i int = int(value)
+	var f float32 = float32(value)
 	fmt.Println(i)
 	fmt.Println(f)
 }
