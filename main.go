b38b28e22b1a1dde40e0c54dd7943149973a45b6
Aashish Raj <iamtheaashish@gmail.com>
2026-09-15 06:17:51 +0530

    refactor main.go to initialize slice x with a length of 5 and remove unnecessary append operations

--- main.go
+++ main.go
@@ -5,17 +5,8 @@ import (
 )
 
 func main(){
-	var x []int
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 10)
-	fmt.Println(x, len(x), cap(x))
+	x := make([]int, 5)
 	x = append(x, 20)
 	fmt.Println(x, len(x), cap(x))
-	x = append(x, 30)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 40)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 50)
-	fmt.Println(x, len(x), cap(x))
-	// slice and capacity
+	
 }
