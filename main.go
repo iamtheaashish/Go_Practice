155ecba84d688f83ee8e98b30566bec1c02a7464
Aashish Raj <iamtheaashish@gmail.com>
2026-09-15 06:15:40 +0530

    refactor main.go to initialize slice x as empty and print its length and capacity at each step

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main(){
-	var x = []int{10, 20, 30}
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
 	x = append(x, 40)
-
-	fmt.Println(x)
-
-
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
+	// slice and capacity
 }
