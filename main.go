45cfac41453de26d0b78db85c5d221b82419c4ba
Aashish Raj <iamtheaashish@gmail.com>
2026-09-15 22:07:48 +0530

    refactor main.go to replace age-based conditions with a switch statement for improved clarity

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	age := 16
-	
-	if age >= 18 {
-		fmt.Println("Person is an adult.")
-	} else if age >= 12 {
-		fmt.Println("Person is a teen")
-	} else if age <= 11 {
-		fmt.Println("Person is a kid")
+	// simple switch
+	i := 5
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	default:
+		fmt.Println("Other")
 	}
 
 }
