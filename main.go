217e7f6ee71960a6b447504027658dad7c9b8916
Aashish Raj <iamtheaashish@gmail.com>
2026-09-16 06:19:40 +0530

    refactor main.go to remove unused variable declarations and streamline array initialization

--- main.go
+++ main.go
@@ -5,20 +5,7 @@ import (
 )
 
 func main() {
-	// arrays
-	// var nums [4]int
-	// nums[0] = 1
-	// fmt.Println(nums)
+	nums := [2][2]int{{3,4},{5,6}}
 
-	var vals [4]bool
-	vals[2] = true
-	vals[1] = true
-	fmt.Println(vals)
-
-	var name [3]string
-	name[0] = "golang"
-	fmt.Println(name)
-
-	nums := [3]int{1,2,3}
 	fmt.Println(nums)
 }
