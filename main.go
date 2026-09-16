13c999e6a711634bba3ac1f52cd2024da339129c
Aashish Raj <iamtheaashish@gmail.com>
2026-09-16 06:16:06 +0530

    refactor main.go to comment out unused array declarations for clarity

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 
 func main() {
 	// arrays
-	var nums [4]int
+	// var nums [4]int
+	// nums[0] = 1
+	// fmt.Println(nums)
 
-	fmt.Println(len(nums))
+	var vals [4]bool
+	vals[2] = true
+	vals[1] = true
+	fmt.Println(vals)
+
+	var name [3]string
+	name[0] = "golang"
+	fmt.Println(name)
+
+	nums := [3]int{1,2,3}
+	fmt.Println(nums)
 }
