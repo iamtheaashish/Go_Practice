19c1f6231917b24ba6e430efe89655fe5f79bd12
Aashish Raj <iamtheaashish@gmail.com>
2026-09-16 06:04:42 +0530

    Nothing much

--- main.go
+++ main.go
@@ -2,27 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	// simple switch
-	// i := 5
+	// arrays
+	var nums [4]int
 
-	// switch i {
-	// case 1:
-	// 	fmt.Println("one")
-	// case 2:
-	// 	fmt.Println("two")
-	// default:
-	// 	fmt.Println("Other")
-	// }
-
-	// multiple condition switch
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's weekend")
-	default:
-		fmt.Println("It's workday")
-	}
+	fmt.Println(len(nums))
 }
