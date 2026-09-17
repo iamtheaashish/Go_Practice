3363bc34d948a4e77f6535deb0acb630a549c257
Aashish Raj <iamtheaashish@gmail.com>
2026-09-17 08:43:14 +0530

    refactor main.go to comment out unused slice operations for clarity

--- main.go
+++ main.go
@@ -7,17 +7,31 @@ func main() {
 	// uninitialized slice is nil
 	
 
-	var nums =  make([]int, 2, 5)
-	fmt.Println(cap(nums))
-	// capacity = maximum numbers of elements can fit.
-	// length = number of ACTUAL elements which are there.
-	fmt.Println(nums == nil)
+	// var nums =  make([]int, 2, 5)
+	// // fmt.Println(cap(nums))
+	// // // capacity = maximum numbers of elements can fit.
+	// // // length = number of ACTUAL elements which are there.
+	// // fmt.Println(nums == nil)
 
-	nums = append(nums, 1)
-	nums = append(nums, 2)
-	nums = append(nums, 3)
-	nums = append(nums, 4)
-	fmt.Println(nums)
-	fmt.Println(cap(nums))
+	// // nums = append(nums, 1)
+	// // nums = append(nums, 2)
+	// // nums = append(nums, 3)
+	// // nums = append(nums, 4)
+	// // fmt.Println(nums)
+	// // fmt.Println(cap(nums))
 
+	// nums[0] = 3
+
+// 	var nums = make([]int, 0, 5)
+// 	nums = append(nums, 2)
+// 	var nums2 = make([]int, len(nums))
+
+// 	copy(nums2, nums)
+
+// 	fmt.Println(nums, nums2)
+// 	fmt.Println(cap(nums), cap(nums2))
+// 	fmt.Println(len(nums), cap(nums2))
+// 
+
+	//
 }
