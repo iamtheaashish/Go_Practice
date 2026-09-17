9796fce4522e2bd0b3f3dc45ad65d3213cbb1418
Aashish Raj <iamtheaashish@gmail.com>
2026-09-17 08:31:41 +0530

    refactor main.go to initialize nums variable and improve slice capacity handling

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import "fmt"
 func main() {
 	// most used construct in go
 	// uninitialized slice is nil
-	var nums []int
+	
 
+	var nums =  make([]int, 2, 5)
+	fmt.Println(cap(nums))
+	// capacity = maximum numbers of elements can fit.
 	fmt.Println(nums == nil)
 
-	fmt.Println(len(nums))
-
-	
+	nums = append(nums, 1)
+	nums = append(nums, 2)
+	nums = append(nums, 3)
+	nums = append(nums, 4)
+	fmt.Println(nums)
+	fmt.Println(cap(nums))
 
 }
