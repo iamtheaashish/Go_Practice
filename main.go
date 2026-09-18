e37205bfa09cbc7e9c90a2ba99b62d008282a896
Aashish Raj <iamtheaashish@gmail.com>
2026-09-18 12:34:37 +0530

    refactor main.go to comment out unused map examples for clarity

--- main.go
+++ main.go
@@ -44,18 +44,43 @@ func main() {
 
 	// fmt.Println(slices.Equal(nums1, nums2))
 
-	var nilMap map[string]int
-	fmt.Println(nilMap)
+	// var nilMap map[string]int
+	// fmt.Println(nilMap)
 
-	totalWins := map[string]int{}
-	fmt.Println(totalWins)
+	// totalWins := map[string]int{}
+	// fmt.Println(totalWins)
 
-	teams := map[string][]string {
-		"Orcas": []string{"Fred", "Ralph", "Bijou"},
-		"Lions": []string{"Sarah", "Peter", "Billie"},
-		"Kittens": []string{"Waldo", "Raul", "Ze"},
+	// teams := map[string][]string {
+	// 	"Orcas": []string{"Fred", "Ralph", "Bijou"},
+	// 	"Lions": []string{"Sarah", "Peter", "Billie"},
+	// 	"Kittens": []string{"Waldo", "Raul", "Ze"},
+	// }
+
+	// fmt.Println(teams)
+
+	// totalWins := map[string]int{}
+	// 	totalWins["Orcas"] = 1
+	// 	totalWins["Lions"] = 2
+	// 	fmt.Println(totalWins["Orcas"])
+	// 	fmt.Println(totalWins["Kittens"])
+	// 	totalWins["Kittens"]++
+
+	// 	fmt.Println(totalWins["Kittens"])
+	// 	totalWins["Lions"] = 3
+	// 	fmt.Println(totalWins["Lions"])
+
+	m := map[string]int{
+		"hello" : 5,
+		"world" : 0,
 	}
 
-	fmt.Println(teams)
+	v, ok := m["hello"]
+	fmt.Println(v, ok)
+	
+	v, ok = m["world"]
+	fmt.Println(v, ok)
 
+	v, ok = m["goodbye"]
+	fmt.Println(v, ok)
+	
 }
