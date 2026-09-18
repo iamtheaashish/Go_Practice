8678366bc5ef286a4eb8f12cedd832af577bf517
Aashish Raj <iamtheaashish@gmail.com>
2026-09-18 12:51:10 +0530

    refactor main.go to comment out unused map examples and improve code clarity

--- main.go
+++ main.go
@@ -69,18 +69,32 @@ func main() {
 	// 	totalWins["Lions"] = 3
 	// 	fmt.Println(totalWins["Lions"])
 
+	// m := map[string]int{
+	// 	"hello" : 5,
+	// 	"world" : 0,
+	// }
+
+	// v, ok := m["hello"]
+	// fmt.Println(v, ok)
+	
+	// v, ok = m["world"]
+	// fmt.Println(v, ok)
+
+	// v, ok = m["goodbye"]
+	// fmt.Println(v, ok)
+
 	m := map[string]int{
 		"hello" : 5,
-		"world" : 0,
+		"world" : 10,
 	}
 
-	v, ok := m["hello"]
-	fmt.Println(v, ok)
-	
-	v, ok = m["world"]
-	fmt.Println(v, ok)
+	fmt.Println(m, len(m))
 
-	v, ok = m["goodbye"]
-	fmt.Println(v, ok)
-	
+	delete(m, "hello")
+
+	fmt.Println(m)
+
+	clear(m)
+
+	fmt.Println(m, len(m))
 }
